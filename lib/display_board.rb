f049b0751ab10318771efffdb27e9e015665c90a
Clint Richards <richardsclint17@gmail.com>
2020-07-31 19:12:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,8 @@
 
-board = [" "," "," "," "," "," "," "," "," "]
-def display_board
-output = capture_puts{ display_board(board) }
-rows = output.split("\n")
-
-expect(rows[0]).to eq("   |   |   ")
-expect(rows[1]).to eq("-----------")
-expect(rows[2]).to eq("   |   |   ")
-expect(rows[3]).to eq("-----------")
-expect(rows[4]).to eq("   |   |   ")
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
